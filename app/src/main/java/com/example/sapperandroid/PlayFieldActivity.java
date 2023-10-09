74efc044d5985de09f3ad674eba141a094537ccf
Baranouski Raman <baranouski.raman@gmail.com>
2023-10-09 21:03:04 +0300

    Completed field

--- app/src/main/java/com/example/sapperandroid/PlayFieldActivity.java
+++ app/src/main/java/com/example/sapperandroid/PlayFieldActivity.java
@@ -1,37 +1,41 @@
 package com.example.sapperandroid;
 
+import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
+import android.util.DisplayMetrics;
 import android.widget.GridView;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-public class PlayFieldActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    private SapperGridAdapter mAdapter;
+public class PlayFieldActivity extends Activity {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_field);
 
-        final GridView g = (GridView) findViewById(R.id.sapperField);
-        mAdapter = new SapperGridAdapter(getApplicationContext(),
-                android.R.layout.simple_list_item_1);
-        g.setAdapter(mAdapter);
-        g.setOnItemSelectedListener(this);
-        g.setOnItemClickListener((parent, v, position, id) -> {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
 
-        });
+        GridView gridview = (GridView) findViewById(R.id.sapperField);
+        gridview.setOnItemClickListener(gridviewOnItemClickListener);
+
+        ImageAdapter adapter = new ImageAdapter(this, width/20, height/10);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            gridview.setNumColumns(width / (width / 10));
+            adapter = new ImageAdapter(this, width / 10, height / 20);
+        } else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            gridview.setNumColumns(width / (width / 20));
+            adapter = new ImageAdapter(this, width / 20, height / 10);
+        }
+
+        gridview.setAdapter(adapter);
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View v, int position,
-                               long id) {
+    private GridView.OnItemClickListener gridviewOnItemClickListener = (parent, v, position, id) -> {
 
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
+    };
 }
